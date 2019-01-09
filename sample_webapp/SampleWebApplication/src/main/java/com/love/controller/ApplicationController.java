9ddffe6c6a7bcebbd001cb9a555d3e1803abbf05
lovetaneja <taneja.love@gmail.com>
2019-01-08 21:36:09 -0800

    Sample Web Application using maven and Spring MVC

---
+++ sample_webapp/SampleWebApplication/src/main/java/com/love/controller/ApplicationController.java
@@ -0,0 +1,23 @@
+package com.love.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+@RequestMapping("/")
+public class ApplicationController {
+    @RequestMapping(value="/Test", method = RequestMethod.GET)
+    public String welcome(ModelMap model) {
+        model.addAttribute("msgArgument", "Maven Java Web Application Project: Success!");
+        return "index";
+    }
+
+    @RequestMapping(value="/Print/{arg}", method = RequestMethod.GET)
+    public String welcomeName(@PathVariable String arg, ModelMap model) {
+        model.addAttribute("msgArgument", "Maven Java Web Application Project, input variable: " + arg);
+        return "index";
+    }
+}
